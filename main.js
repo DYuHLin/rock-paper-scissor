be8b3b0e9e4a318921c3361f3a5d076c33a6403c
DYuHLin <damianver2000@gmail.com>
2022-10-19 17:33:19 +0200

    added the looping function so that the game plays 5 times

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let playerOption;
 let computerOption;
 let options = ['rock', 'paper', 'scissors']
 
+//variables for score 
+let playerScore = 0;
+let computerScore = 0;
+
 //this function chooses a random number between 0 - 2 and assigns that number to the array which
 //then is assigned to the computerOption variable
 
@@ -13,32 +17,46 @@ function getComputerChoice(){
 };
 
 //this function cycles through the outcomes of the game determined by the rules of rock paper scissors
-//and what the user and computer chose
+//and what the user and computer chose. Also increments score based on who wins
 
 function playRound(playerOption, computerOption){
 
     if(playerOption === "rock" && computerOption === "scissors"){
-        return "You win! Rock beats Scissors"
+        playerScore += 1
+        return "You win! Rock beats Scissors"      
+
     } else if (playerOption === "rock" && computerOption === "paper"){
+        computerScore += 1
         return "You lose! Paper beats Rock"
+
     } else if (playerOption === "rock" && computerOption === "rock"){
         return "Draw! Try again"
+        
     } else if (playerOption === "scissors" && computerOption === "paper"){
+        playerScore += 1
         return "You win! Scissors beats Paper"
+
     } else if(playerOption === "scissors" && computerOption === "rock"){
+        computerScore += 1
         return "You lose! Rock beats Scissors"
+
     } else if (playerOption === "scissors" && computerOption === "scissors"){
         return "Draw! Try again"
+
     } else if (playerOption === "paper" && computerOption === "rock"){
+        playerScore += 1
         return "You win! Paper beats Rock"
+
     } else if(playerOption === "paper" && computerOption === "scissors"){
+        computerScore += 1
         return "You lose! Scissors beats Paper"
+        
     } else if (playerOption === "paper" && computerOption === "paper"){
         return "Draw! Try again"
     }
 }
 
-//the game function loops the game until either the user or computer reach a score of 5
+//the game function loops the game 5 times
 
 function game(){
     for(i = 0; i < 5; i++){
@@ -46,7 +64,17 @@ function game(){
         getComputerChoice()
 
         console.log(playRound(playerOption, computerOption))
+
     }
+    //tells you who won based on score
+    if(playerScore > computerScore){
+        console.log("Player Wins " + playerScore + " : " + computerScore)
+    } else if(playerScore < computerScore){
+        console.log("Computer Wins " + playerScore + " : " + computerScore)
+    } else {
+        console.log("draw")
+    }
+    
 }
 
 game()
