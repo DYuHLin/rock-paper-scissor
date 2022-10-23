bc2dfbac3691ac440ae96abc9403976348dba863
DYuHLin <damianver2000@gmail.com>
2022-10-23 15:02:02 +0200

    added a UI and made the game so that the first person to 5 wins and to play a new game reload the page

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 This will be a project displaying the JS knowledge I have learnt so far by creating the game of rock,
 paper, scissors. For now, this project will not have any GUI and will be working from the console.
+
+# credit
+https://www.flaticon.com/free-icons
+
+1. Rock icons created by Freepik - Flaticon
+
+2. Files and folders icons created by Vector Stall - Flaticon
+
+3. Scissors icons created by Freepik - Flaticon
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//html elements
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissor = document.querySelector('.scissor')
+
+const user = document.querySelector('.user')
+const computer = document.querySelector('.computer')
+const score = document.querySelector('.score')
+const outcome = document.querySelector('.outcome')
+
 //the variables for player, computer and options
 let playerOption;
 let computerOption;
@@ -14,6 +24,16 @@ function getComputerChoice(){
     let randomComputerOption = Math.floor(Math.random() * 3)
 
     computerOption = options[randomComputerOption]
+
+    if(computerOption === 'rock'){
+        computer.innerHTML = '<img class="weapon" src="img/rock.png" alt="a rock">'
+    } 
+    else if(computerOption === 'paper'){
+        computer.innerHTML = '<img class="weapon" src="img/sheet.png" alt="a paper">'
+    } 
+    else if(computerOption === 'scissors'){
+        computer.innerHTML = '<img class="weapon" src="img/scissors.png" alt="a scissor">'
+    }
 };
 
 //this function cycles through the outcomes of the game determined by the rules of rock paper scissors
@@ -23,67 +43,104 @@ function playRound(playerOption, computerOption){
 
     if(playerOption === "rock" && computerOption === "scissors"){
         playerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML= "You win! Rock beats Scissors" 
         return "You win! Rock beats Scissors"      
 
     } else if (playerOption === "rock" && computerOption === "paper"){
         computerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "You lose! Paper beats Rock" 
         return "You lose! Paper beats Rock"
 
     } else if (playerOption === "rock" && computerOption === "rock"){
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "Draw! Try again" 
         return "Draw! Try again"
         
     } else if (playerOption === "scissors" && computerOption === "paper"){
         playerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "You win! Scissors beats Paper" 
         return "You win! Scissors beats Paper"
 
     } else if(playerOption === "scissors" && computerOption === "rock"){
         computerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "You lose! Rock beats Scissors"
         return "You lose! Rock beats Scissors"
 
     } else if (playerOption === "scissors" && computerOption === "scissors"){
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "Draw! Try again" 
         return "Draw! Try again"
 
     } else if (playerOption === "paper" && computerOption === "rock"){
         playerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "You win! Paper beats Rock" 
         return "You win! Paper beats Rock"
 
     } else if(playerOption === "paper" && computerOption === "scissors"){
         computerScore += 1
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "You lose! Scissors beats Paper" 
         return "You lose! Scissors beats Paper"
         
     } else if (playerOption === "paper" && computerOption === "paper"){
-        console.log(playerScore + ":" + computerScore)
+        score.innerHTML = `${playerScore} : ${computerScore}`
+        outcome.innerHTML = "Draw! Try again" 
         return "Draw! Try again"
     }
 }
 
-//the game function loops the game 5 times
+//tells you who won based on score
+function finalScore(){
+    if(playerScore === 5){
+        rock.disabled = true
+        paper.disabled = true
+        scissor.disabled = true
+        outcome.innerHTML = "You Win the game! Reload to start a new one!"
+    } 
+    else if(computerScore === 5){
+        rock.disabled = true
+        paper.disabled = true
+        scissor.disabled = true
+        outcome.innerHTML = "The Computer Win the game! Reload to start a new one!"
+    }
+}
+
+//the game function plays the game and gets a winner when a player reaches 5
 
 function game(){
-    for(i = 0; i < 5; i++){
-        playerOption = prompt("Choose between rock, paper or scissors: ").toLocaleLowerCase()
+    rock.addEventListener('click', (e) => {
+        playerOption = 'rock'
+        console.log(playerOption)
+        user.innerHTML = '<img class="weapon" src="img/rock.png" alt="a rock">'
         getComputerChoice()
-
         console.log(playRound(playerOption, computerOption))
+        finalScore()
+    })
 
-    }
-    //tells you who won based on score
-    if(playerScore > computerScore){
-        console.log("Player Wins " + playerScore + " : " + computerScore)
-    } else if(playerScore < computerScore){
-        console.log("Computer Wins " + playerScore + " : " + computerScore)
-    } else {
-        console.log("draw")
-    }
-    
+    paper.addEventListener('click', (e) => {
+        user.innerHTML = ''
+        playerOption = 'paper'
+        console.log(playerOption)
+        user.innerHTML = '<img class="weapon" src="img/sheet.png" alt="a paper">'
+        getComputerChoice()
+        console.log(playRound(playerOption, computerOption))
+        finalScore()
+    })
+
+    scissor.addEventListener('click', (e) => {
+        user.innerHTML = ''
+        playerOption = 'scissors'
+        console.log(playerOption)
+        user.innerHTML = '<img class="weapon" src="img/scissors.png" alt="a scissor">'
+        getComputerChoice()
+        console.log(playRound(playerOption, computerOption))
+        finalScore()
+    })   
 }
 
 game()
